ab55160fd370ba8c1ed73da6732fec6bd07632c8
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-01-22 09:30:08 +0300

    add change
    --search text is displayed in url

--- src/pages/HomePage/SearchContainer/SearchContainer.jsx
+++ src/pages/HomePage/SearchContainer/SearchContainer.jsx
@@ -1,18 +1,20 @@
-
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import CSSModules from 'react-css-modules';
 
 import SearchInput from '_components/SearchInput';
-
 import { setVisibilityFilter } from '_actions/books';
 
+import styles from './SearchContainer.scss';
+
+@CSSModules(styles, { allowMultiple: true })
 class SearchContainer extends PureComponent {
-  componentDidMount() {
-    const { location, history, filterValue } = this.props;
-    if (filterValue) {
-      location.search = `/?search=${filterValue}`;
+  updateUrl(valueInput) {
+    const { location, history } = this.props;
+    if (valueInput) {
+      location.search = `/?search=${valueInput}`;
     } else {
       location.search = '/?';
     }
@@ -22,21 +24,22 @@ class SearchContainer extends PureComponent {
   handleChangeFilter = (e) => {
     const valueInput = e.target.value;
     const { onChangeFilter } = this.props;
+    this.updateUrl(valueInput);
     onChangeFilter(valueInput);
   }
 
   render() {
     const { filterValue } = this.props;
+
     return (
-      <div>
-        <form >
-          <h4>Поиск</h4>
+      <div styleName="root">
+        <div styleName="containerSearch">
           <SearchInput
             onChange={this.handleChangeFilter}
             value={filterValue}
             placeholder="Что ищем?"
           />
-        </form>
+        </div>
       </div>
     );
   }
