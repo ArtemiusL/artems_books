d73a6d1e9774c42fcc7502a78e3f4e83d5c3ef01
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-01-22 09:31:57 +0300

    add change
    --added link to homepage

--- src/pages/BookCard/BookCard.jsx
+++ src/pages/BookCard/BookCard.jsx
@@ -1,8 +1,12 @@
 import React, { PureComponent } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import CSSModules from 'react-css-modules';
 
+import styles from './BookCard.scss';
+
+@CSSModules(styles, { allowMultiple: true })
 class BookCard extends PureComponent {
   static propTypes = {
     match: PropTypes.object.isRequired,
@@ -14,11 +18,20 @@ class BookCard extends PureComponent {
     const book = books.filter(item => (Number(match.params.id) === item.id));
 
     return book.map(item => (
-      <div key={item.id}>
-        <span>{item.title}</span> <br />
-        <span>{item.author}</span> <br />
-        <span>{item.description}</span> <br />
-        <img src={item.imgUrl} alt="картинка" />
+      <div styleName="root">
+        <div key={item.id} styleName="card">
+          <div styleName="cardOptions">
+            <span>Книга: {item.title}</span> <br />
+            <span>Автор: {item.author}</span> <br />
+            <span>Описание: {item.description}</span> <br />
+          </div>
+          <div styleName="blockImage">
+            <img styleName="picture" src={item.imgUrl} alt="картинка" />
+          </div>
+        </div>
+        <div styleName="blockLink">
+          <Link to="/" styleName="link">На главную</Link>
+        </div>
       </div>
     ),
     );
